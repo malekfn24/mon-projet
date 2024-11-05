936f9452ff62a465d29ce7796b0dd8174fa9075b
malekfn24 <malek.fennia@esprit.tn>
2024-11-05 14:18:58 +0100

    Modification du fichier TpFoyerApplication

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,12 +1,10 @@
 package tn.esprit.tpfoyer;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 
 @SpringBootApplication
 @EnableScheduling
@@ -15,6 +13,7 @@ public class TpFoyerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TpFoyerApplication.class, args);
+        System.out.println("L'application TpFoyer a démarré avec succès !");
     }
 
 }
